a759787697ad778709cb32cd7281a6916371af5b
Gleison Silva <121878359+GleisonDev@users.noreply.github.com>
2025-09-30 16:17:03 -0300

    Commit inicial
    Criação do projeto

---
+++ backend/src/test/java/com/sireostech/vstore/gestao/VstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sireostech.vstore.gestao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
